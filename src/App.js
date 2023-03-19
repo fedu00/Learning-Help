a59017d86055fd75e19f62221a8f8ac8c77a17e7
Wojtek F <fedakwojciech@gmail.com>
2023-03-19 16:57:54 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import NotePage from "./views/NotePage/NotePage";
 import PomodorePage from "./views/PomodorePage/PomodorePage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // useLocation,
+} from "react-router-dom";
 import MainTemplate from "./components/MainTemplate/MainTemplate";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
 
 function App() {
+  // const location = useLocation();
   return (
     <Provider store={store}>
-      <Router>
-        <MainTemplate>
-          <Routes>
+      {/* <Router> */}
+      <MainTemplate>
+        <div>lalalaalalalalall</div>
+        {/* <Routes>
             <Route path="/Learning-Help/" element={<NotePage />} />
             <Route path="/pomodore" element={<PomodorePage />} />
-          </Routes>
-        </MainTemplate>
-      </Router>
+          </Routes> */}
+      </MainTemplate>
+      {/* </Router> */}
     </Provider>
   );
 }
